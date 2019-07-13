7f1075f779a8f3e3d2356522fef5f7b3432c436c
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-13 17:17:53 -0600

    Agrupar Media Queries con mqpacker

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-    plugins: [
-        // require('autoprefixer')({
-        //     grid:false
-        // }),
-        require('postcss-import')({
-            plugins: [
-              require("stylelint")({ /* your options */ })
-            ]
-          }),
-        require('postcss-font-magician'),
-        require('postcss-cssnext')({
-			features: {
-				autoprefixer: {
-                    grid: false,
-                    flexbox: false
-                },           
-            customProperties: false,
-            calc: false,
-            }
-		})
-    ]
+  plugins: [
+    // require('autoprefixer')({
+    //     grid:false
+    // }),
+    require('postcss-import')({
+      plugins: [
+        require("stylelint")({ /* your options */ })
+      ]
+    }),
+    require('postcss-font-magician'),
+    require('postcss-cssnext')({
+      features: {
+        autoprefixer: {
+          grid: false,
+          flexbox: false
+        },
+        customProperties: false,
+        calc: false,
+      }
+    }),
+    require('css-mqpacker')
+  ]
 }
